b6b8da28ea3e8eccea6c9fbfef27c9825e488987
Аргунов Федор Михайлович <fargunov@internet.ru>
2023-02-26 17:05:05 +0900

    Step 3. Добавить в модель userprofile номер телефона

--- online_shop/app_users/models.py
+++ online_shop/app_users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class UserProfile(models.Model):
@@ -7,8 +8,9 @@ class UserProfile(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     city = models.CharField(max_length=36, blank=True)
     balance = models.IntegerField(default=0, verbose_name='Balance')
-    avatar = models.FileField(blank=True, verbose_name='аватарка профиля', upload_to='images/avatars')
+    avatar = models.ImageField(blank=True, verbose_name='аватарка профиля', upload_to='images/avatars')
     patronymic = models.CharField(max_length=100, verbose_name='Отчество')
+    phone_number = PhoneNumberField(null=True, blank=True, unique=True, verbose_name='Номер телефона')
 
     class Meta:
         db_table = 'UserProfile'
